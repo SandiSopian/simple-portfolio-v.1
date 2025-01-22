9529e0c96d0545e08faf75f81a3e7c6a212c975d
Sandi Sopian <shunasand73@gmail.com>
2025-01-22 14:39:58 +0700

    add database.js

---
+++ simple-portfolio-v1/src/services/database.js
@@ -0,0 +1,83 @@
+import BrandImg1 from "../src/assets/icons/coca-cola.svg";
+import BrandImg2 from "../src/assets/icons/lg.svg";
+import BrandImg3 from "../src/assets/icons/hyundai.svg";
+import BrandImg4 from "../src/assets/icons/kia-motors.svg";
+import BrandImg5 from "../src/assets/icons/samsung.svg";
+import BrandImg6 from "../src/assets/icons/line.svg";
+import ProjectImg1 from "../src/assets/icons/calendar.svg";
+import ProjectImg2 from "../src/assets/icons/creative-jigsaw.svg";
+import ProjectImg3 from "../src/assets/icons/app-development.svg";
+import ProjectImg4 from "../src/assets/icons/monitor.svg";
+import ContactImg from "../src/assets/icons/handshake.svg";
+
+export const header = {
+  email: "sandis17@outlook.com",
+  cv: "https://www.canva.com/design/DAGZ55bg9WI/3f6UodohV1UdpV70PQunTg/edit?utm_content=DAGZ55bg9WI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+};
+
+// Hero
+export const hero = {
+  lastestShots: "https://ss-portfolio-34bab.web.app/",
+};
+
+// Brand
+export const brand = [
+  {
+    brandImg: BrandImg1,
+  },
+  {
+    brandImg: BrandImg2,
+  },
+  {
+    brandImg: BrandImg3,
+  },
+  {
+    brandImg: BrandImg4,
+  },
+  {
+    brandImg: BrandImg5,
+  },
+  {
+    brandImg: BrandImg6,
+  },
+];
+
+// Project
+export const project = [
+  {
+    images: ProjectImg1,
+    projectTitle: "UI & UX",
+    projectDescription: "Designing a user interface and user experience for a website or app.",
+  },
+  {
+    images: ProjectImg2,
+    projectTitle: "Web & Mobile App",
+    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+  },
+  {
+    images: ProjectImg3,
+    projectTitle: "Design & Creative",
+    projectDescription: "Creating visual concepts to communicate ideas that inspire, inform, and captivate consumers.",
+  },
+  {
+    images: ProjectImg4,
+    projectTitle: "Development",
+    projectDescription: "Developing a website or app that is user-friendly and responsive.",
+  },
+];
+
+// Contact
+export const contact = {
+  contactLogo: ContactImg,
+  contactDescription: "Tell me about your project and let's work together.",
+  contactEmail: "sandis17@outlook.com",
+  contactWhatsapp: "+6281234567890",
+};
+
+// Footer
+export const footer = {
+  footerDescription: "© 2025 SS. All rights reserved.",
+  footerLinkdin: "https://www.linkedin.com/in/sandis17/",
+  footerBehance: "https://www.linkedin.com/in/sandis17/",
+  footerInstagram: "https://www.linkedin.com/in/sandis17/",
+};
